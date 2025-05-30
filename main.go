96e7358d414c0ebde8e633f46f9fe50ec6c83fe6
brkq <bbburkeq@gmail.com>
2025-05-30 23:25:36 +0300

    Домашка этап 2

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
+import "fmt"
+
 func main(){
 	const USDtoEUR = 0.88
 	const USDtoRUB = 78.63
 	const EURtoRUB = USDtoEUR / USDtoRUB
+	fmt.Println("___ Конвертер валют ___")
+	fmt.Println(getUserData())
+	calcCurrencyToCurrency(1, "asd", "asd")
+}
+
+func getUserData() int{
+	var amountCurrency int
+	fmt.Print("Введите количество валюты: ")
+	fmt.Scan(&amountCurrency)
+	return amountCurrency
+}
+
+func calcCurrencyToCurrency(amountCurrency int, currency1, currency2 string) int{
+	return 0
 }
