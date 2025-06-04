9fd7edf76062304935dc14e5825c0c9d550c949b
brkq <bbburkeq@gmail.com>
2025-06-04 19:27:55 +0300

    Разбиение на пакеты

--- 3-struct/main.go
+++ 3-struct/main.go
@@ -1,1 +1,19 @@
-package struct3
+package bins
+
+import "time"
+
+type Bin struct{
+	id string
+	private bool
+	createdAt time.Time
+	name string
+}
+func newBin(id, name string, private bool, createdAt time.Time) *Bin{
+	return &Bin{
+		id: id,
+		name: name,
+		private: private,
+		createdAt: createdAt,
+	}
+}
+var BinList = make([]Bin, 0, 20)
